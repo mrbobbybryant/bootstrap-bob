7a8463c8ecf5bf29dfa5fd74f1eae59ca28c4938
Bobby Bryant <bobby@hatrackmedia.com>
2015-04-01 12:25:11 -0400

    wire theme options page with homepage template part.

--- content-home.php
+++ content-home.php
@@ -1,14 +1,17 @@
  <?php
-
+$options = get_option( 'bootstrap_bob_settings' );
+$lead_in = $options['bootstrap_bob_text_field_0'];
+$intro_heading = $options['bootstrap_bob_text_field_1'];
+$cta_button = $options['bootstrap_bob_text_field_2'];
  ?>
 
 <!-- Header -->
     <header class="header-bg">
         <div class="container">
             <div class="intro-text">
-                <div class="intro-lead-in">Welcome To Our Studio!</div>
-                <div class="intro-heading">It is Nice To Meet You</div>
-                <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
+                <div class="intro-lead-in"><?php echo $lead_in; ?></div>
+                <div class="intro-heading"><?php echo $intro_heading; ?></div>
+                <a href="#services" class="page-scroll btn btn-xl"><?php echo $cta_button; ?></a>
             </div>
         </div>
     </header>
