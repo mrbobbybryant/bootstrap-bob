46dcc3d120af3ce9cd61c1219f2e1435dc27762d
Bobby Bryant <bobby@hatrackmedia.com>
2015-04-02 16:56:17 -0400

    add clients cpt and template part.

---
+++ content-clients.php
@@ -0,0 +1,21 @@
+<?php
+
+	$clients= bootstrap_bob_clients();
+	if ( $clients->have_posts() ) : ?>
+
+	<!-- Clients Aside -->
+    <aside class="clients">
+        <div class="container">
+            <div class="row">
+            	<?php while ( $clients->have_posts() ) : $clients->the_post(); ?>
+            	<?php $client_logo = get_the_post_thumbnail( get_the_ID(), 'full', array( 'class' => 'img-responsive' )); ?>
+                <div class="col-md-3 col-sm-6">
+                    <a href="#">
+                        <?php echo $client_logo; ?>
+                    </a>
+                </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </aside>
+<?php endif;    
---
+++ inc/cpt-clients.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Register Clients Post Type
+ */
+function bootstrap_bob_client_post_type() {
+	
+	$singular = 'Client';
+	$plural = 'Clients';
+
+	$labels = array(
+		'name' 			=> $plural,
+		'singular_name' 	=> $singular,
+		'add_new' 		=> 'Add New',
+		'add_new_item'  	=> 'Add New ' . $singular,
+		'edit'		        => 'Edit',
+		'edit_item'	        => 'Edit ' . $singular,
+		'new_item'	        => 'New ' . $singular,
+		'view' 			=> 'View ' . $singular,
+		'view_item' 		=> 'View ' . $singular,
+		'search_term'   	=> 'Search ' . $plural,
+		'parent' 		=> 'Parent ' . $singular,
+		'not_found' 		=> 'No ' . $plural .' found',
+		'not_found_in_trash' 	=> 'No ' . $plural .' in Trash'
+		);
+
+	$args = array(
+		'labels'              => $labels,
+	        'public'              => true,
+	        'publicly_queryable'  => true,
+	        'exclude_from_search' => false,
+	        'show_in_nav_menus'   => true,
+	        'show_ui'             => true,
+	        'show_in_menu'        => true,
+	        'show_in_admin_bar'   => true,
+	        'menu_position'       => 10,
+	        'menu_icon'           => 'dashicons-universal-access',
+	        'can_export'          => true,
+	        'delete_with_user'    => false,
+	        'hierarchical'        => false,
+	        'has_archive'         => true,
+	        'query_var'           => true,
+	        'capability_type'     => 'post',
+	        'map_meta_cap'        => true,
+	        // 'capabilities' => array(),
+	        'rewrite'             => array( 
+	        	'slug' => $plural,
+	        	'with_front' => true,
+	        	'pages' => true,
+	        	'feeds' => true,
+
+	        ),
+	        'supports'            => array( 
+	        	'title',
+	        	'thumbnail',
+	        )
+	);
+	register_post_type( $singular, $args );
+}
+
+add_action( 'init', 'bootstrap_bob_client_post_type' );
