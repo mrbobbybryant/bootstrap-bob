8042abb1a39c6679aaa07ce0a50086de63a874b8
Bobby Bryant <bobby@hatrackmedia.com>
2015-05-09 22:24:23 -0400

    replace get option calls with get theme mods

--- content-home.php
+++ content-home.php
@@ -1,17 +1,19 @@
  <?php
-$options = get_option( 'bootstrap_bob_settings' );
-$lead_in = $options['bootstrap_bob_text_field_0'];
-$intro_heading = $options['bootstrap_bob_text_field_1'];
-$cta_button = $options['bootstrap_bob_text_field_2'];
- ?>
+/**
+ * The template for displaying all single posts.
+ *
+ * @package bootstrap_bob
+ */
+
+?>
 
 <!-- Header -->
     <header>
         <div class="container">
             <div class="intro-text">
-                <div class="intro-lead-in"><?php echo esc_html( $lead_in ); ?></div>
-                <div class="intro-heading"><?php echo esc_html( $intro_heading ); ?></div>
-                <a href="#services" class="page-scroll btn btn-xl"><?php echo esc_html( $cta_button ); ?></a>
+                <div class="intro-lead-in"><?php echo esc_html( get_theme_mod( 'hero_title_one_textbox', 'Welcome To Our Studio!' ) ); ?></div>
+                <div class="intro-heading"><?php echo esc_html( get_theme_mod( 'hero_title_two_textbox', 'IT IS NICE TO MEET YOU' ) ); ?></div>
+                <a href="#services" class="page-scroll btn btn-xl"><?php echo esc_html( get_theme_mod( 'hero_button_textbox', 'Tell Me More' ) ); ?></a>
             </div>
         </div>
     </header>
